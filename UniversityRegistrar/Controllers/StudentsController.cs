f5d0b1ede79bc625d33370dd4734e4c668313762
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-05 13:15:11 -0800

    add sudent controller and view folders
    Co-authored-by: Brianca Knight <briancaknight@gmail.com>

---
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly UniversityRegistrarContext _db;
+
+    public StudentsController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+  }
+}
---
+++ UniversityRegistrar/Models/Student.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace UniversityRegistrar.Models
+{
+  public class Student
+  {
+    public int StudentId { get; set; }
+
+    public string StudentName { get; set; }
+
+    public DateTime Enrollment { get; }
+  }
+}
