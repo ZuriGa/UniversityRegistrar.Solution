e2daf0424f0c45822209a93b3313c372840faeab
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-05 14:39:32 -0800

    add courses model + controller + view
    Co-authored-by: Brianca Knight <briancaknight@gmail.com>

---
+++ UniversityRegistrar/Models/Course.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UniversityRegistrar.Models
+{
+  public class Course
+  {
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    
+    public string CourseNumber { get; set; }
+  }
+}
