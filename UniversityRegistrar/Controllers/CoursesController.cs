24084a138f013b680ff9ddc8d7880f68174d036f
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-05 15:02:08 -0800

    add edit and delete actions + view
    Co-authored-by: Brianca Knight <briancaknight@gmail.com>

--- UniversityRegistrar/Controllers/CoursesController.cs
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -43,5 +43,34 @@ namespace UniversityRegistrar.Controllers
       Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
       return View(thisCourse);
     }
+
+    public ActionResult Edit(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Course course)
+    {
+      _db.Courses.Update(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      _db.Courses.Remove(thisCourse);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
