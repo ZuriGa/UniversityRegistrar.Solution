968510c7f79737ccec1619557c9eb2c7b0a7a731
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-06 11:22:17 -0800

    update splash page with database details
    Co-authored-by: Brianca Knight <briancaknight@gmail.com>

--- UniversityRegistrar/Controllers/HomeController.cs
+++ UniversityRegistrar/Controllers/HomeController.cs
@@ -1,13 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UniversityRegistrar.Controllers
 {
   public class HomeController : Controller
   {
+  private readonly UniversityRegistrarContext _db;
+
+  public HomeController(UniversityRegistrarContext db)
+  {
+    _db = db;
+  }
+  
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Course[] courses = _db.Courses.ToArray();
+      Student[] students = _db.Students.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("courses", courses);
+      model.Add("students", students);
+      return View(model);
     }
   }
 }
